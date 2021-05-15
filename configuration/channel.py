84f8f8c9eaac25e08dc7a097c6ddc003d4c261e2
nickk2002 <nicolae.filat@yahoo.com>
2021-05-15 11:50:01 +0300

    Removed src folder and moved the configuration folder

---
+++ configuration/channel.py
@@ -0,0 +1,9 @@
+from xknx.devices import Sensor, Switch, BinarySensor
+
+
+class Channel:
+    def __init__(self, sensor: Sensor, binary_sensor: BinarySensor, switch: Switch, index):
+        self.sensor = sensor
+        self.binary_sensor = binary_sensor
+        self.switch = switch
+        self.index = index
--- src/configuration/channel.py
+++
@@ -1,9 +0,0 @@
-from xknx.devices import Sensor,Switch,BinarySensor
-
-
-class Channel:
-    def __init__(self, sensor : Sensor, binary_sensor : BinarySensor, switch : Switch, index):
-        self.sensor = sensor
-        self.binary_sensor = binary_sensor
-        self.switch = switch
-        self.index = index
